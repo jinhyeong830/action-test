28d0f509be4420bf9b7a764da51ed9a816950345
jinhyeong830 <shg02387@naver.com>
2025-04-16 11:10:27 +0900

    change action

--- src/main/java/com/example/plog/PlogApplication.java
+++ src/main/java/com/example/plog/PlogApplication.java
@@ -12,16 +12,19 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class PlogApplication {
 	
 	public static void main(String[] args) {
-
+        String profile = System.getenv("SPRING_PROFILES_ACTIVE");
 		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-        System.setProperty("SERVER_PORT", dotenv.get("SERVER_PORT"));
-        System.setProperty("MANAGER_PORT", dotenv.get("MANAGER_PORT"));
+        if ("local".equals(profile)) {
         System.setProperty("DB_DEV_USERNAME", dotenv.get("DB_DEV_USERNAME"));
         System.setProperty("DB_DEV_PASSWORD", dotenv.get("DB_DEV_PASSWORD"));
         System.setProperty("DB_DEV_URL", dotenv.get("DB_DEV_URL"));
+        }else{
         System.setProperty("DB_PROD_USERNAME", dotenv.get("DB_PROD_USERNAME"));
         System.setProperty("DB_PROD_PASSWORD", dotenv.get("DB_PROD_PASSWORD"));
         System.setProperty("DB_PROD_URL", dotenv.get("DB_PROD_URL"));
+        }
+        System.setProperty("MANAGER_PORT", dotenv.get("MANAGER_PORT"));
+        System.setProperty("SERVER_PORT", dotenv.get("SERVER_PORT"));
         System.setProperty("JWT_ISSUER", dotenv.get("JWT_ISSUER"));
         System.setProperty("JWT_SECRET_KEY", dotenv.get("JWT_SECRET_KEY"));
 
