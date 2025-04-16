188dcfa268c8445b40842f183f44e41038734ab0
jinhyeong830 <shg02387@naver.com>
2025-04-16 12:53:59 +0900

    another npe..

--- src/main/java/com/example/plog/config/DotenvConfig.java
+++ src/main/java/com/example/plog/config/DotenvConfig.java
@@ -10,17 +10,25 @@ import org.springframework.context.annotation.Configuration;
 public class DotenvConfig {
     @PostConstruct
     public void loadEnv() {
+        String profile = System.getenv("SPRING_PROFILES_ACTIVE");
+        System.out.println("profile: "+profile);
         Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-        System.out.println(System.getProperty("DB_DEV_PASSWORD"));
-        System.setProperty("SERVER_PORT", dotenv.get("SERVER_PORT"));
-        System.setProperty("MANAGER_PORT", dotenv.get("MANAGER_PORT"));
+        if ("local".equals(profile)) {
         System.setProperty("DB_DEV_USERNAME", dotenv.get("DB_DEV_USERNAME"));
         System.setProperty("DB_DEV_PASSWORD", dotenv.get("DB_DEV_PASSWORD"));
         System.setProperty("DB_DEV_URL", dotenv.get("DB_DEV_URL"));
-        // System.setProperty("DB_PROD_USERNAME", dotenv.get("DB_PROD_USERNAME"));
-        // System.setProperty("DB_PROD_PASSWORD", dotenv.get("DB_PROD_PASSWORD"));
-        // System.setProperty("DB_PROD_URL", dotenv.get("DB_PROD_URL"));
+        System.setProperty("SERVER_PORT", dotenv.get("SERVER_PORT"));
+        System.setProperty("MANAGER_PORT", dotenv.get("MANAGER_PORT"));
         System.setProperty("JWT_ISSUER", dotenv.get("JWT_ISSUER"));
         System.setProperty("JWT_SECRET_KEY", dotenv.get("JWT_SECRET_KEY"));
+        }else{
+        System.setProperty("DB_PROD_USERNAME", dotenv.get("DB_PROD_USERNAME"));
+        System.setProperty("DB_PROD_PASSWORD", dotenv.get("DB_PROD_PASSWORD"));
+        System.setProperty("DB_PROD_URL", dotenv.get("DB_PROD_URL"));
+        System.setProperty("SERVER_PORT", dotenv.get("SERVER_PORT"));
+        System.setProperty("MANAGER_PORT", dotenv.get("MANAGER_PORT"));
+        System.setProperty("JWT_ISSUER", dotenv.get("JWT_ISSUER"));
+        System.setProperty("JWT_SECRET_KEY", dotenv.get("JWT_SECRET_KEY"));
+        }
     }
 }
