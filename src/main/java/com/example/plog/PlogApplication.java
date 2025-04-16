fd0a46da7a5dd329cee0e62842829f65e44c3227
jinhyeong830 <shg02387@naver.com>
2025-04-16 12:30:25 +0900

    env 관련 설정 변경

--- src/main/java/com/example/plog/PlogApplication.java
+++ src/main/java/com/example/plog/PlogApplication.java
@@ -23,11 +23,12 @@ public class PlogApplication {
         System.setProperty("JWT_ISSUER", dotenv.get("JWT_ISSUER"));
         System.setProperty("JWT_SECRET_KEY", dotenv.get("JWT_SECRET_KEY"));
         }else{
-		Dotenv dotenv = Dotenv.configure().directory("deploy").ignoreIfMissing().load();
+		Dotenv dotenv = Dotenv.configure().directory("/home/ubuntu/p-log/P-log_backend/deploy").ignoreIfMissing().load();
 
         System.setProperty("DB_PROD_USERNAME", dotenv.get("DB_PROD_USERNAME"));
         System.setProperty("DB_PROD_PASSWORD", dotenv.get("DB_PROD_PASSWORD"));
         System.setProperty("DB_PROD_URL", dotenv.get("DB_PROD_URL"));
+            System.out.println(dotenv.get("DB_PROD_URL"));
             System.setProperty("MANAGER_PORT", dotenv.get("MANAGER_PORT"));
         System.setProperty("SERVER_PORT", dotenv.get("SERVER_PORT"));
         System.setProperty("JWT_ISSUER", dotenv.get("JWT_ISSUER"));
